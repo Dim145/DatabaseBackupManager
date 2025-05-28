ff5f7b52f123e5fa29071debd2056ff26b11ed19
dim145 <dimitri.bleach@gmail.com>
2025-05-28 18:51:40 +0200

    fix sqlite dates

--- DatabaseBackupManager/Data/Sqlite/SqliteContext.cs
+++ DatabaseBackupManager/Data/Sqlite/SqliteContext.cs
@@ -1,4 +1,5 @@
-﻿using DatabaseBackupManager.Services.StorageService;
+﻿using Core.Models;
+using DatabaseBackupManager.Services.StorageService;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseBackupManager.Data.Sqlite;
@@ -7,4 +8,68 @@ public class SqliteContext(
     DbContextOptions<SqliteContext> options,
     IConfiguration conf,
     IStorageService storageService)
-    : BaseContext(options, conf, storageService);
+    : BaseContext(options, conf, storageService)
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<BackupJob>(entity =>
+        {
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("datetime()")
+                .HasConversion(
+                    v => v.Ticks,
+                    v => new DateTime(v))
+                .ValueGeneratedOnAdd();
+
+            entity.Property(e => e.UpdatedAt)
+                .HasDefaultValueSql("datetime()")
+                .HasConversion(
+                    v => v.Ticks,
+                    v => new DateTime(v))
+                .ValueGeneratedOnAddOrUpdate();
+        });
+        
+        modelBuilder.Entity<Server>(entity =>
+        {
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("datetime()")
+                .HasConversion(
+                    v => v.Ticks,
+                    v => new DateTime(v))
+                .ValueGeneratedOnAdd();
+
+            entity.Property(e => e.UpdatedAt)
+                .HasDefaultValueSql("datetime()")
+                .HasConversion(
+                    v => v.Ticks,
+                    v => new DateTime(v))
+                .ValueGeneratedOnAddOrUpdate();
+        });
+        
+        modelBuilder.Entity<Backup>(entity =>
+        {
+            entity.Property(e => e.CreatedAt)
+                .HasDefaultValueSql("datetime()")
+                .HasConversion(
+                    v => v.Ticks,
+                    v => new DateTime(v))
+                .ValueGeneratedOnAdd();
+
+            entity.Property(e => e.UpdatedAt)
+                .HasDefaultValueSql("datetime()")
+                .HasConversion(
+                    v => v.Ticks,
+                    v => new DateTime(v))
+                .ValueGeneratedOnAddOrUpdate();
+            
+            entity.Property(e => e.BackupDate)
+                .HasConversion(
+                    v => v.Ticks,
+                    v => new DateTime(v))
+                .HasDefaultValueSql("datetime()")
+                .ValueGeneratedOnAdd();
+        });
+    }
+}
