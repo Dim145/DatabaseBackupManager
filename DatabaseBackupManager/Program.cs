f6a4bf8455e3d24876f8fd5e34095342c6db28ec
dim145 <dimitri.bleach@gmail.com>
2025-05-28 19:27:18 +0200

    ?

--- DatabaseBackupManager/Program.cs
+++ DatabaseBackupManager/Program.cs
@@ -229,6 +229,8 @@ await context.Database.MigrateAsync();
 await services.SeedDatabase();
 await HangfireService.InitHangfireRecurringJob(context, builder.Configuration);
 
+AppContext.SetSwitch("Microsoft.EntityFrameworkCore.Issue32911", true);
+
 try
 {
     app.Run();
